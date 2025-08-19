c956fa3d1c93f0ecacd11579887f3df7b18cedf6
Diego do Bomfim da Silva <desenvolvimento@Mac-mini-de-Diego.local>
2025-08-19 17:30:55 -0300

    Corrigindo erro do host

--- src/Facades/RabbitLib.php
+++ src/Facades/RabbitLib.php
@@ -8,6 +8,11 @@
 
     class RabbitLib extends Facade
     {
+        public static string $host = '';
+        public static string $port = '';
+        public static string $user = '';
+        public static string $password = '';
+        public static string $vhost = '/';
         public static string $queue = '';
 
         public static function __callStatic($method, $parameters)
